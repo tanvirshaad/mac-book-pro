757c60ad03cbce9f514dc7e009b0ac60e3dec1ed
tanvirshaad <tanvirshaad@gmail.com>
2021-08-21 19:06:18 +0600

    updateCost and updateTotal done

--- js/app.js
+++ js/app.js
@@ -1,28 +1,59 @@
+const totalPrice = document.getElementById('total-price');
+const memoryCost = document.getElementById('memory-cost');
+const storageCost = document.getElementById('storage-cost');
+const deliveryCost = document.getElementById('delivery-cost');
+
+//update cost
 function updateCost(id, price) {
     const changingId = document.getElementById(id);
     changingId.innerText = price;
+    // if (price != 0) {
+    //     totalPrice.innerText = totalPriceValue + price;
+    // }
+    // return totalPrice;
 }
+//update total
+function updateTotal(price) {
+    const memoryCostValue = parseInt(memoryCost.innerText);
+    const storageCostValue = parseInt(storageCost.innerText);
+    const deliveryCostValue = parseInt(deliveryCost.innerText);
+    const total = memoryCostValue + storageCostValue + deliveryCostValue;
+    totalPrice.innerText = total + 1299;
+}
+
+//adding event listener and calling function
 document.getElementById('memory-8gb').addEventListener('click', function() {
     updateCost('memory-cost', 0);
+    updateTotal();
 });
 document.getElementById('memory-16gb').addEventListener('click', function() {
     updateCost('memory-cost', 180);
+    updateTotal();
 });
+
 document.getElementById('ssd-256gb').addEventListener('click', function() {
     updateCost('storage-cost', 0);
+    updateTotal();
 });
 document.getElementById('ssd-512gb').addEventListener('click', function() {
-    updateCost('storage-cost', 400);
+    updateCost('storage-cost', 100);
+    updateTotal();
+
 });
 document.getElementById('ssd-1tb').addEventListener('click', function() {
-    console.log('1tb clicked');
+    updateCost('storage-cost', 180);
+    updateTotal();
 });
 document.getElementById('free-delivery').addEventListener('click', function() {
-    console.log('free clicked');
+    updateCost('delivery-cost', 0);
+    updateTotal();
 });
 document.getElementById('early-delivery').addEventListener('click', function() {
-    console.log('early clicked');
+    updateCost('delivery-cost', 20);
+    updateTotal();
 });
+
+//promo code
 document.getElementById('promo-apply').addEventListener('click', function() {
     const promoInput = document.getElementById('promo-input');
     const promoCode = promoInput.value;
